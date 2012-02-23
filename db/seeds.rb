bac9468b742e55ebff652be7bd39a182dee9f4f3
Craig Read <craigabread@gmail.com>
2012-02-23 12:32:52 +1100

    Putting the site pages re-organisation into the seeds.db file

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,97 +36,58 @@ def create_default_slots
 end
 
 def create_default_pages
-  Page.create(:name => 'home', :title => 'Home', :navlabel => 'Home',
-              :position => 0, :admin => false, :parent => nil,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => 'h1. Welcome to the raids per loot system!')
-  Page.create(:name => 'links', :title => 'Links', :navlabel => 'Links',
-              :position => 1, :admin => false, :parent => nil, :redirect => true,
-              :controller_name => 'links', :action_name => 'list',
-              :body => '.')
-  Page.create(:name => 'raids', :title => 'Raids', :navlabel => 'Raids',
-              :position => 2, :admin => false, :parent => nil,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
-  Page.create(:name => 'players', :title => 'Players', :navlabel => 'Players',
-              :position => 3, :admin => false, :parent => nil,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
-  Page.create(:name => 'drops', :title => 'Drops', :navlabel => 'Drops',
-              :position => 4, :admin => false, :parent => nil,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
+  # Home Page and Sub Pages
+  home_page = Page.create(:name => 'home', :title => 'Home', :navlabel => 'Home',
+                          :position => 0, :admin => false, :redirect => false,
+                          :body => 'h1. Welcome to the raids per loot system!')
+  home_page.children.create(:name => 'links', :title => 'Links', :navlabel => 'Links',
+                            :position => 0, :admin => false, :redirect => true,
+                            :controller_name => 'links', :action_name => 'list', :body => '.')
 
+  # Raiding Page and Sub Pages
+  raiding_page = Page.create(:name => 'raiding', :title => 'Raiding', :navlabel => 'Raiding',
+                             :position => 1, :admin => false,
+                             :body => 'This is the raiding section of the site')
+  raiding_page.children.create(:name => 'raids', :title => 'Raids', :navlabel => 'Raids',
+                               :position => 0, :admin => false, :body => '.')
+  raiding_page.children.create(:name => 'zones', :title => 'Zones', :navlabel => 'Zones',
+                               :position => 1, :admin => false, :body => '.')
+  raiding_page.children.create(:name => 'mobs', :title => 'Mobs', :navlabel => 'Mobs',
+                               :position => 2, :admin => false, :body => '.')
+
+  # Players Page and Sub Pages
+  players_page = Page.create(:name => 'players_menu', :title => 'Players', :navlabel => 'Players',
+                             :position => 2, :admin => false, :body => 'This is the players section of the site')
+  players_page.children.create(:name => 'players', :title => 'Characters', :navlabel => 'Characters',
+                               :position => 0, :admin => false, :body => '.')
+  players_page.children.create(:name => 'archetypes', :title => 'Classes', :navlabel => 'Classes',
+                               :position => 1, :admin => false, :body => '.')
+  players_page.children.create(:name => 'ranks', :title => 'Ranks', :navlabel => 'Ranks',
+                               :position => 2, :admin => false, :body => '.')
+
+  # Loot Page and Sub Pages
+  loot_page = Page.create(:name => 'loot', :title => 'Loot', :navlabel => 'Loot',
+                          :position => 3, :admin => false, :body => 'This is the loot section of the site')
+  loot_page.children.create(:name => 'drops', :title => 'Drops', :navlabel => 'Drops',
+                            :position => 0, :admin => false, :body => '.')
+  loot_page.children.create(:name => 'loot_types', :title => 'Types', :navlabel => 'Types',
+                            :position => 1, :admin => false, :body => '.')
+  loot_page.children.create(:name => 'slots', :title => 'Slots', :navlabel => 'Slots',
+                            :position => 2, :admin => false, :body => '.')
+  loot_page.children.create(:name => 'items', :title => 'Items', :navlabel => 'Items',
+                            :position => 3, :admin => false, :body => '.')
+
+  # Admin Page and Sub Pages
   admin_page = Page.create!(:name => 'admin', :title => 'Site Admin', :navlabel => 'Site Admin',
-                            :position => 5, :admin => true, :parent => nil,
-                            :redirect => false, :controller_name => nil, :action_name => nil,
-                            :body => 'Here are the various pages for administering this site.
-
-Content allows you to modify, add and delete pages on this site.
-You need to take care here so you don' 't break navigation to the admin functionality!
-
-User Admin will allow you to maintain users.
-
-Links and Link Categories allow you to maintain links to external sites.
-')
-  Page.create(:name => 'pages', :title => 'Content Management', :navlabel => 'Content',
-              :position => 0, :admin => true, :parent => admin_page,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
-  Page.create(:name => 'users', :title => 'User Management', :navlabel => 'Users',
-              :position => 1, :admin => true, :parent => admin_page,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
-  Page.create(:name => 'link_categories', :title => 'Link Categories', :navlabel => 'Link Categories',
-              :position => 2, :admin => true, :parent => admin_page,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
-  Page.create(:name => 'links', :title => 'Links', :navlabel => 'Links',
-              :position => 3, :admin => true, :parent => admin_page,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
-
-  loot_admin = Page.create!(:name => 'loot_admin', :title => 'Loot Admin', :navlabel => 'Loot Admin',
-                            :position => 6, :admin => true, :parent => nil,
-                            :redirect => false, :controller_name => nil, :action_name => nil,
-                            :body => 'Here is where you can edit elements of the loot system.
-
-Raid Zones is where you can enter the list of raid zones you will get loot from.
-Mobs is where you can enter details of the named mobs that drop loot.
-Player classes is for entering classes or archetypes that players belong to, and loot is for.
-Player Ranks is where you enter player ranks, such as "Main", "Raid Alt" and "General Alt".
-Players is obviously, where you enter players, their archetypes and ranks.
-Loot Types is where you can categorize loot into Armour, Jewelery, etc, etc.
-Loot Items is where you can enter the actual details of a particular item, what classes it is for and the slots it can go into.
-Loot Slots is where you maintain the various places you can equip loot.')
-  Page.create(:name => 'zones', :title => 'Raid Zones', :navlabel => 'Raid Zones',
-              :position => 1, :admin => true, :parent => loot_admin,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
-  Page.create(:name => 'mobs', :title => 'Mobs', :navlabel => 'Mobs',
-              :position => 2, :admin => true, :parent => loot_admin,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
-  Page.create(:name => 'archetypes', :title => 'Player Classes', :navlabel => 'Player Classes',
-              :position => 3, :admin => true, :parent => loot_admin,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
-  Page.create(:name => 'ranks', :title => 'Player Ranks', :navlabel => 'Player Ranks',
-              :position => 4, :admin => true, :parent => loot_admin,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
-  Page.create(:name => 'loot_types', :title => 'Loot Types', :navlabel => 'Loot Types',
-              :position => 5, :admin => true, :parent => loot_admin,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
-  Page.create(:name => 'slots', :title => 'Loot Slots', :navlabel => 'Loot Slots',
-              :position => 6, :admin => true, :parent => loot_admin,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
-  Page.create(:name => 'items', :title => 'Loot Items', :navlabel => 'Loot Items',
-              :position => 7, :admin => true, :parent => loot_admin,
-              :redirect => false, :controller_name => nil, :action_name => nil,
-              :body => '.')
+                            :position => 4, :admin => true, :body => 'This is the admin section of the site')
+  admin_page.children.create(:name => 'pages', :title => 'Content Management', :navlabel => 'Content',
+                             :position => 0, :admin => true, :body => '.')
+  admin_page.children.create(:name => 'links', :title => 'Links', :navlabel => 'Links',
+                             :position => 1, :admin => true, :body => '.')
+  admin_page.children.create(:name => 'link_categories', :title => 'Link Categories', :navlabel => 'Link Categories',
+                             :position => 2, :admin => true, :body => '.')
+  admin_page.children.create(:name => 'users', :title => 'User Management', :navlabel => 'Users',
+                             :position => 3, :admin => true, :body => '.')
 end
 
 def create_default_loot_types
