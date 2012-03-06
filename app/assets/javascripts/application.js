105b39fcc384ebd92a1b76cec7a93c739a696f67
Craig Read <craigabread@gmail.com>
2012-03-06 14:29:45 +1100

    Restoring javascript functionality in Heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,8 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//= require_self
 //= require jquery
 //= require jquery_ujs
+//= require controls
 //= require_tree .
