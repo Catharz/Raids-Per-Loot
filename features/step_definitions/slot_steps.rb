dc352012f3871a3bf512511d17fbb80b52587d90
Craig Read <craigabread@gmail.com>
2012-03-16 18:36:57 +1100

    Change Slots to use dataTables

--- features/step_definitions/slot_steps.rb
+++ features/step_definitions/slot_steps.rb
@@ -4,7 +4,7 @@ end
 
 When /^I delete the (\d+)(?:st|nd|rd|th) slot$/ do |pos|
   visit slots_path
-  within("table tr:nth-child(#{pos.to_i+1})") do
+  within("table tr:nth-child(#{pos.to_i})") do
     click_link "Destroy"
   end
 end
