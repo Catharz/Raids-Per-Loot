4d7d464a54d46404eb271cfe658d5298fe5fd362
Craig Read <craigabread@gmail.com>
2012-09-10 18:08:13 +1000

    UI Updates

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -49,5 +49,21 @@ describe Character do
         unknown.archetype_name.should eq 'Unknown'
       end
     end
+
+    context "#archetype_root" do
+      it "should show the root archetype name when it has one" do
+        fighter = FactoryGirl.create(:archetype, name: 'Fighter')
+        brawler = FactoryGirl.create(:archetype, name: 'Brawler', parent_id: fighter.id)
+        character = FactoryGirl.create(:character, valid_character_attributes.merge!(name: 'brawler', archetype_id: brawler.id))
+
+        character.archetype_root.should eq 'Fighter'
+      end
+
+      it "should show Unknown if it does not have an archetype" do
+        unknown = Character.new
+
+        unknown.archetype_root.should eq 'Unknown'
+      end
+    end
   end
 end
