cdb5c72d493bcfa5ed86e80e04c40145f052d9c2
Craig Read <craigabread@gmail.com>
2012-12-05 14:35:49 +1100

    Resolved an endless loop caused by the items observer

--- app/observers/item_observer.rb
+++ app/observers/item_observer.rb
@@ -2,7 +2,6 @@ class ItemObserver < ActiveRecord::Observer
   observe Item
 
   def after_save(item)
-    item.fetch_soe_item_details
     item.drops.each do |drop|
       drop.update_attribute(:loot_type, item.loot_type) unless item.loot_type.nil? or drop.loot_type.eql? item.loot_type
       if item.loot_type.name.eql? "Trash"
