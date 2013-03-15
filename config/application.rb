37ae84633694f0feac08b8181f102e6c84714605
Craig Read <craigabread@gmail.com>
2013-03-15 19:35:33 +1100

    Updating config to ignore observers when running rake db:migrate

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,11 @@ module RaidsPerLoot
 
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-    config.active_record.observers = :character_observer, :drop_observer, :attendance_observer, :loot_type_observer, :item_observer
+
+    # HACK, HACK, HACK!!!  Observers break CI
+    unless File.basename($0) == 'rake' && ARGV.include?('db:migrate')
+      config.active_record.observers = :character_observer, :drop_observer, :attendance_observer, :loot_type_observer, :item_observer
+    end
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
