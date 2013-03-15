4987ff6a5a9d1d7b1b0b667e303389af6ecdbffc
Craig Read <craigabread@gmail.com>
2013-03-15 23:28:33 +1100

    Changing the travis build script to use 'rake db:schema:load'

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module RaidsPerLoot
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
     # HACK, HACK, HACK!!!  Observers break CI
-    unless File.basename($0) == 'rake' && ARGV.include?('db:migrate')
+    unless File.basename($0) == 'rake' && (ARGV.include?('db:migrate') || ARGV.include?('db:schema:load'))
       config.active_record.observers = :character_observer, :drop_observer, :attendance_observer, :loot_type_observer, :item_observer
     end
 
