f39f60e19c4cbc6c1a49fea37a378a025d03f01f
Craig Read <craigabread@gmail.com>
2013-05-08 17:39:54 +1000

    Coverage for UsersController

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe UsersController do
   fixtures :users
@@ -7,83 +7,79 @@ describe UsersController do
     login_as :quentin
   end
 
-  it 'allows signup' do
-    lambda do
-      create_user
-      response.should be_redirect
-    end.should change(User, :count).by(1)
-  end
-
-  
-
-
-  it 'requires login on signup' do
-    lambda do
-      create_user(:login => nil)
-      assigns[:user].errors.get(:login).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
-  
-  it 'requires password on signup' do
-    lambda do
-      create_user(:password => nil)
-      assigns[:user].errors.get(:password).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
-  
-  it 'requires password confirmation on signup' do
-    lambda do
-      create_user(:password_confirmation => nil)
-      assigns[:user].errors.get(:password_confirmation).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
-
-  it 'requires email on signup' do
-    lambda do
-      create_user(:email => nil)
-      assigns[:user].errors.get(:email).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
-  
-  
-  
   def create_user(options = {})
     post :create, :user => { :login => 'quire', :email => 'quire@example.com',
-      :password => 'quire69', :password_confirmation => 'quire69' }.merge(options)
+                             :password => 'quire69', :password_confirmation => 'quire69' }.merge(options)
   end
-end
 
-describe UsersController do
-  describe "route recognition and generation" do
-    it "should generate params for users's index action from GET /users" do
+  describe 'signing up' do
+    it 'allows signup' do
+      lambda do
+        create_user
+        response.should be_redirect
+      end.should change(User, :count).by(1)
+    end
+
+    it 'requires login on signup' do
+      lambda do
+        create_user(:login => nil)
+        assigns[:user].errors.get(:login).should_not be_nil
+        response.should be_success
+      end.should_not change(User, :count)
+    end
+
+    it 'requires password on signup' do
+      lambda do
+        create_user(:password => nil)
+        assigns[:user].errors.get(:password).should_not be_nil
+        response.should be_success
+      end.should_not change(User, :count)
+    end
+
+    it 'requires password confirmation on signup' do
+      lambda do
+        create_user(:password_confirmation => nil)
+        assigns[:user].errors.get(:password_confirmation).should_not be_nil
+        response.should be_success
+      end.should_not change(User, :count)
+    end
+
+    it 'requires email on signup' do
+      lambda do
+        create_user(:email => nil)
+        assigns[:user].errors.get(:email).should_not be_nil
+        response.should be_success
+      end.should_not change(User, :count)
+    end
+
+  end
+
+  describe 'route recognition and generation' do
+    it 'should generate params for users index action from GET /users' do
       {:get => '/users'}.should route_to(:controller => 'users', :action => 'index')
       {:get => '/users.xml'}.should route_to(:controller => 'users', :action => 'index', :format => 'xml')
       {:get => '/users.json'}.should route_to(:controller => 'users', :action => 'index', :format => 'json')
     end
     
-    it "should generate params for users's new action from GET /users" do
+    it 'should generate params for users new action from GET /users' do
       {:get => '/users/new'}.should route_to(:controller => 'users', :action => 'new')
       {:get => '/users/new.xml'}.should route_to(:controller => 'users', :action => 'new', :format => 'xml')
       {:get => '/users/new.json'}.should route_to(:controller => 'users', :action => 'new', :format => 'json')
     end
     
-    it "should generate params for users's create action from POST /users" do
+    it 'should generate params for users create action from POST /users' do
       {:post => '/users'}.should route_to(:controller => 'users', :action => 'create')
       {:post => '/users.xml'}.should route_to(:controller => 'users', :action => 'create', :format => 'xml')
       {:post => '/users.json'}.should route_to(:controller => 'users', :action => 'create', :format => 'json')
     end
     
-    it "should generate params for users's show action from GET /users/1" do
+    it 'should generate params for users show action from GET /users/1' do
       {:get => '/users/1'}.should route_to(:controller => 'users', :action => 'show', :id => '1')
       {:get => '/users/1.xml'}.should route_to(:controller => 'users', :action => 'show', :id => '1', :format => 'xml')
       {:get => '/users/1.json'}.should route_to(:controller => 'users', :action => 'show', :id => '1', :format => 'json')
     end
     
-    it "should generate params for users's edit action from GET /users/1/edit" do
+    it 'should generate params for users edit action from GET /users/1/edit' do
       {:get => '/users/1/edit'}.should route_to(:controller => 'users', :action => 'edit', :id => '1')
     end
     
@@ -93,39 +89,169 @@ describe UsersController do
       {:put => '/users/1.json'}.should route_to(:controller => 'users', :action => 'update', :id => '1', :format => 'json')
     end
     
-    it "should generate params for users's destroy action from DELETE /users/1" do
+    it 'should generate params for users destroy action from DELETE /users/1' do
       {:delete => '/users/1'}.should route_to(:controller => 'users', :action => 'destroy', :id => '1')
       {:delete => '/users/1.xml'}.should route_to(:controller => 'users', :action => 'destroy', :id => '1', :format => 'xml')
       {:delete => '/users/1.json'}.should route_to(:controller => 'users', :action => 'destroy', :id => '1', :format => 'json')
     end
   end
   
-  describe "named routing" do
+  describe 'named routing' do
     before(:each) do
       get :new
     end
     
     it "should route users_path() to /users" do
-      users_path().should == "/users"
-      controller.users_path(:format => 'xml').should == "/users.xml"
-      controller.users_path(:format => 'json').should == "/users.json"
+      users_path().should == '/users'
+      controller.users_path(:format => 'xml').should == '/users.xml'
+      controller.users_path(:format => 'json').should == '/users.json'
     end
     
     it "should route new_user_path() to /users/new" do
-      new_user_path().should == "/users/new"
-      controller.new_user_path(:format => 'xml').should == "/users/new.xml"
-      controller.new_user_path(:format => 'json').should == "/users/new.json"
+      new_user_path().should == '/users/new'
+      controller.new_user_path(:format => 'xml').should == '/users/new.xml'
+      controller.new_user_path(:format => 'json').should == '/users/new.json'
     end
     
     it "should route user_(:id => '1') to /users/1" do
-      user_path(:id => '1').should == "/users/1"
-      controller.user_path(:id => '1', :format => 'xml').should == "/users/1.xml"
-      controller.user_path(:id => '1', :format => 'json').should == "/users/1.json"
+      user_path(:id => '1').should == '/users/1'
+      controller.user_path(:id => '1', :format => 'xml').should == '/users/1.xml'
+      controller.user_path(:id => '1', :format => 'json').should == '/users/1.json'
     end
     
     it "should route edit_user_path(:id => '1') to /users/1/edit" do
-      controller.edit_user_path(:id => '1').should == "/users/1/edit"
+      controller.edit_user_path(:id => '1').should == '/users/1/edit'
     end
   end
-  
+
+  describe 'controller actions' do
+    describe 'GET index' do
+      it 'assigns all users as @users' do
+        user = User.create! FactoryGirl.attributes_for(:user)
+
+        get :index
+
+        assigns(:users).should include(user)
+      end
+    end
+
+    describe 'GET show' do
+      it 'assigns the requested user as @user' do
+        user = User.create! FactoryGirl.attributes_for(:user)
+        get :show, :id => user.id.to_s
+        assigns(:user).should eq(user)
+      end
+    end
+
+    describe 'GET new' do
+      it 'assigns a new user as @user' do
+        get :new
+        assigns(:user).should be_a_new(User)
+      end
+    end
+
+    describe 'GET edit' do
+      it 'assigns the requested user as @user' do
+        user = User.create! FactoryGirl.attributes_for(:user)
+        get :edit, :id => user.id.to_s
+        assigns(:user).should eq(user)
+      end
+    end
+
+    describe 'POST create' do
+      describe 'with valid params' do
+        it 'creates a new User' do
+          expect {
+            post :create, :user => FactoryGirl.attributes_for(:user)
+          }.to change(User, :count).by(1)
+        end
+
+        it 'assigns a newly created user as @user' do
+          post :create, :user => FactoryGirl.attributes_for(:user)
+          assigns(:user).should be_a(User)
+          assigns(:user).should be_persisted
+        end
+
+        it 'redirects to the home page' do
+          post :create, :user => FactoryGirl.attributes_for(:user)
+          response.should redirect_to('/')
+        end
+      end
+
+      describe 'with invalid params' do
+        it 'assigns a newly created but unsaved user as @user' do
+          # Trigger the behavior that occurs when invalid params are submitted
+          User.any_instance.stub(:save).and_return(false)
+          post :create, :user => {}
+          assigns(:user).should be_a_new(User)
+        end
+
+        it "re-renders the 'new' template" do
+          # Trigger the behavior that occurs when invalid params are submitted
+          User.any_instance.stub(:save).and_return(false)
+          post :create, :user => {}
+          response.should render_template('new')
+        end
+      end
+    end
+
+    describe 'PUT update' do
+      describe 'with valid params' do
+        it 'updates the requested user' do
+          user = User.create! FactoryGirl.attributes_for(:user)
+          # Assuming there are no other users in the database, this
+          # specifies that the User created on the previous line
+          # receives the :update_attributes message with whatever params are
+          # submitted in the request.
+          User.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
+          put :update, :id => user.id, :user => {'these' => 'params'}
+        end
+
+        it "assigns the requested user as @user" do
+          user = User.create! FactoryGirl.attributes_for(:user)
+          put :update, :id => user.id, :user => FactoryGirl.attributes_for(:user)
+          assigns(:user).should eq(user)
+        end
+
+        it 'redirects to the user' do
+          user = User.create! FactoryGirl.attributes_for(:user)
+          put :update, :id => user.id, :user => FactoryGirl.attributes_for(:user)
+          response.should redirect_to(user)
+        end
+      end
+
+      describe 'with invalid params' do
+        it 'assigns the user as @user' do
+          user = User.create! FactoryGirl.attributes_for(:user)
+          # Trigger the behavior that occurs when invalid params are submitted
+          User.any_instance.stub(:save).and_return(false)
+          put :update, :id => user.id.to_s, :user => {}
+          assigns(:user).should eq(user)
+        end
+
+        it "re-renders the 'edit' template" do
+          user = User.create! FactoryGirl.attributes_for(:user)
+          # Trigger the behavior that occurs when invalid params are submitted
+          User.any_instance.stub(:save).and_return(false)
+          put :update, :id => user.id.to_s, :user => {}
+          response.should render_template('edit')
+        end
+      end
+    end
+
+    describe 'DELETE destroy' do
+      it 'destroys the requested user' do
+        user = User.create! FactoryGirl.attributes_for(:user)
+        expect {
+          delete :destroy, :id => user.id.to_s
+        }.to change(User, :count).by(-1)
+      end
+
+      it 'redirects to the users list' do
+        user = User.create! FactoryGirl.attributes_for(:user)
+        delete :destroy, :id => user.id.to_s
+        response.should redirect_to(users_url)
+      end
+    end
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,33 @@
 FactoryGirl.define do
+  sequence :user_name do |n|
+    "User #{n}"
+  end
+  sequence :user_login do |n|
+    "user_#{n}"
+  end
+  sequence :user_password do |n|
+    "Password#{n}!?!"
+  end
+  sequence :user_password_confirmation do |n|
+    "Password#{n}!?!"
+  end
+  sequence :user_email do |n|
+    "user#{n}@example.com"
+  end
+
   factory :user do |f|
-    f.name "Freddy Krueger"
-    f.login "freddyishere"
-    f.password "password"
-    f.email "whoever@wherever.com"
+    f.name { generate(:user_name) }
+    f.login { generate(:user_login) }
+    f.password { generate(:user_password) }
+    f.password_confirmation { generate(:user_password_confirmation) }
+    f.email { generate(:user_email) }
+  end
+
+  factory :invalid_user, parent: :user do |f|
+    f.name nil
+    f.login nil
+    f.password nil
+    f.password_confirmation nil
+    f.email nil
   end
 end
