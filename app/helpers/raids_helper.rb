2be2284f24a617e19afd1952ce83a35daf543c86
Craig Read <craigabread@gmail.com>
2013-07-24 12:33:32 +1000

    Resolving issue with drops edit screen caused by the raids select box

--- app/helpers/raids_helper.rb
+++ app/helpers/raids_helper.rb
@@ -5,10 +5,15 @@ module RaidsHelper
     else
       selected_date = 'Select Raid'
     end
-    haml_tag :select, :id => "drop_raid_id", :include_blank => 'Select Raid' do
+    haml_tag :select, :id => 'drop_raid_id', :include_blank => 'Select Raid' do
       haml_tag :option, 'Select Raid', :id => '0', :value => '0', :selected => (selected_date == 'Select Raid')
       Raid.order('raid_date desc').each do |raid|
-        haml_tag :option, raid.raid_date, :id => raid.id, :value => raid.id, :selected => ((selected_date != 'Select Raid') and Date.parse(selected_date) == drop.drop_time.to_date)
+        haml_tag :option,
+                 raid.raid_date,
+                 :id => raid.id,
+                 :value => raid.id,
+                 :selected => ((selected_date != 'Select Raid') and
+                     selected_date == drop.drop_time.to_date)
       end
     end
   end
