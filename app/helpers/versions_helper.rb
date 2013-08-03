5f905b03a2d92028b712deef97cfd495ba50d644
Craig Read <craigabread@gmail.com>
2013-08-03 17:35:35 +1000

    Resolving an issue with presenting the version history for new items being added.

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,9 +1,16 @@
 module VersionsHelper
   def changes(old_item, new_item, excluded_columns = %w{updated_at})
-    differences = old_item.attributes.diff(new_item.attributes)
     changed_values = []
-    differences.keys.reject { |k| excluded_columns.include? k }.each do |key|
-      changed_values << {key: key, old: old_item[key], new: new_item[key]}
+    if old_item.nil?
+      differences = new_item.attributes
+      differences.keys.reject { |k| excluded_columns.include? k }.each do |key|
+        changed_values << {key: new_item[key], old: nil, new: new_item[key]}
+      end
+    else
+      differences = old_item.attributes.diff(new_item.attributes)
+      differences.keys.reject { |k| excluded_columns.include? k }.each do |key|
+        changed_values << {key: key, old: old_item[key], new: new_item[key]}
+      end
     end
     changed_values
   end
