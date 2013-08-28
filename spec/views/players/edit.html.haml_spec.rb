5afc90dbfaac1577e9b089807ae8c452ff7a8950
Craig Read <craigabread@gmail.com>
2013-08-29 08:29:43 +1000

    Cleaning up view specs

--- spec/views/players/edit.html.haml_spec.rb
+++ spec/views/players/edit.html.haml_spec.rb
@@ -1,26 +1,28 @@
 require 'spec_helper'
 require 'authentication_spec_helper'
 
-describe "players/edit.html.haml" do
+describe 'players/edit.html.haml' do
   include AuthenticationSpecHelper
   fixtures :users, :services
 
   before(:each) do
     login_as :admin
     @player = assign(:player, stub_model(Player,
-      :name => "MyString"
+      name: 'MyString'
     ))
-    archetype = stub_model(Archetype, :name => 'Archetype')
-    char1 = stub_model(Character, :name => 'Character 1', :player => @player, :archetype => archetype)
-    char2 = stub_model(Character, :name => 'Character 2', :player => @player, :archetype => archetype)
+    archetype = stub_model(Archetype, name: 'Archetype')
+    char1 = stub_model(Character, name: 'Character 1',
+                       player: @player, archetype: archetype)
+    char2 = stub_model(Character, name: 'Character 2',
+                       player: @player, archetype: archetype)
     @player.characters << [char1, char2]
   end
 
-  it "renders the edit player form" do
+  it 'renders the edit player form' do
     render
 
-    assert_select "form", :action => players_path(@player), :method => "post" do
-      assert_select "input#player_name", :name => "player[name]"
+    assert_select 'form', action: players_path(@player), method: 'post' do
+      assert_select 'input#player_name', name: 'player[name]'
     end
   end
 end
