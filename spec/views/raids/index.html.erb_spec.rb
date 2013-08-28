84847c5d160bcc0856e56f7756a72d7ff3150855
Craig Read <craigabread@gmail.com>
2013-08-29 08:18:59 +1000

    Cleaning up view specs

--- spec/views/raids/index.html.erb_spec.rb
+++ spec/views/raids/index.html.erb_spec.rb
@@ -9,21 +9,18 @@ describe 'raids/index.html.erb' do
   before(:each) do
     login_as :admin
     prog = stub_model(RaidType, name: 'Progression')
-    assign(:raids, [
-        stub_model(Raid, :raid_date => Date.parse('01/01/2012'), raid_type: prog),
-        stub_model(Raid, :raid_date => Date.parse('02/01/2012'), raid_type: prog),
-        stub_model(Raid, :raid_date => Date.parse('03/01/2012'), raid_type: prog),
-        stub_model(Raid, :raid_date => Date.parse('04/01/2012'), raid_type: prog),
-        stub_model(Raid, :raid_date => Date.parse('05/01/2012'), raid_type: prog),
-        stub_model(Raid, :raid_date => Date.parse('06/01/2012'), raid_type: prog),
-        stub_model(Raid, :raid_date => Date.parse('07/01/2012'), raid_type: prog)
-    ].paginate(:page => 1, :per_page => 5))
+    raids = Array.new(7) { |n|
+      stub_model(Raid,
+                 raid_date: Date.parse('01/01/2012') + n.days,
+                 raid_type: prog)
+    }
+    assign(:raids, raids.paginate(:page => 1, :per_page => 5))
   end
 
-  it 'renders a list of raids' do
+  it 'renders a paginated list of raids' do
     render
 
-    assert_select 'tr>td', text: 'Progression'
-    assert_select 'tr>td', text: '2012-01-01'
+    assert_select 'tr>td', text: 'Progression', count: 5
+    assert_select 'tr>td', text: '2012-01-01', count: 1
   end
 end
--- spec/views/raids/new.html.erb_spec.rb
+++ spec/views/raids/new.html.erb_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
-describe "raids/new.html.erb" do
+describe 'raids/new.html.erb' do
   before(:each) do
     assign(:raid, stub_model(Raid).as_new_record)
   end
 
-  it "renders new raid form" do
+  it 'renders new raid form' do
     render
 
-    assert_select "form", :action => raids_path, :method => "post" do
+    assert_select 'form', :action => raids_path, :method => 'post' do
+      assert_select 'input#datepicker', name: 'raid[raid_date]'
+      assert_select 'select#raid_raid_type_id', name: 'raid[raid_type_id]'
     end
   end
 end
