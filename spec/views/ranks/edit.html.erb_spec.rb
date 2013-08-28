67c6766326f7db660f8178418dbb6806828d42e9
Craig Read <craigabread@gmail.com>
2013-08-29 08:07:13 +1000

    Cleaning up view specs

--- spec/views/ranks/edit.html.erb_spec.rb
+++ spec/views/ranks/edit.html.erb_spec.rb
@@ -1,20 +1,19 @@
 require 'spec_helper'
 
-describe "ranks/edit.html.erb" do
+describe 'ranks/edit.html.erb' do
   before(:each) do
     @rank = assign(:rank, stub_model(Rank,
-      :name => "MyString",
-      :priority => 1
+      :name => 'Associate',
+      :priority => 2
     ))
   end
 
-  it "renders the edit rank form" do
+  it 'renders the edit rank form' do
     render
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => ranks_path(@rank), :method => "post" do
-      assert_select "input#rank_name", :name => "rank[name]"
-      assert_select "input#rank_priority", :name => "rank[priority]"
+    assert_select 'form', :action => ranks_path(@rank), :method => 'post' do
+      assert_select 'input#rank_name', :name => 'rank[name]'
+      assert_select 'input#rank_priority', :name => 'rank[priority]'
     end
   end
 end
--- spec/views/ranks/new.html.erb_spec.rb
+++ spec/views/ranks/new.html.erb_spec.rb
@@ -1,20 +1,19 @@
 require 'spec_helper'
 
-describe "ranks/new.html.erb" do
+describe 'ranks/new.html.erb' do
   before(:each) do
     assign(:rank, stub_model(Rank,
-      :name => "MyString",
+      :name => 'Member',
       :priority => 1
     ).as_new_record)
   end
 
-  it "renders new rank form" do
+  it 'renders new rank form' do
     render
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => ranks_path, :method => "post" do
-      assert_select "input#rank_name", :name => "rank[name]"
-      assert_select "input#rank_priority", :name => "rank[priority]"
+    assert_select 'form', :action => ranks_path, :method => 'post' do
+      assert_select 'input#rank_name', :name => 'rank[name]'
+      assert_select 'input#rank_priority', :name => 'rank[priority]'
     end
   end
 end
