8fa3361b9c505ad4ee318867e8dab07eefa562e6
Craig Read <craigabread@gmail.com>
2013-08-29 08:51:55 +1000

    Cleaning up view specs

--- spec/views/mobs/index.html.erb_spec.rb
+++ spec/views/mobs/index.html.erb_spec.rb
@@ -1,30 +1,29 @@
 require 'spec_helper'
 
-describe "mobs/index.html.erb" do
+describe 'mobs/index.html.erb' do
   before(:each) do
-    easy = stub_model(Difficulty, :name => 'Easy', :rating => 1)
-    normal = stub_model(Difficulty, :name => 'Normal', :rating => 2)
-    hard = stub_model(Difficulty, :name => 'Hard', :rating => 3)
+    easy = stub_model(Difficulty, name: 'Easy', rating: 1)
+    normal = stub_model(Difficulty, name: 'Normal', rating: 2)
+    hard = stub_model(Difficulty, name: 'Hard', rating: 3)
     assign(:difficulties, [easy, normal, hard])
     assign(:mobs, [
         stub_model(Mob,
-                   :name => "Name",
-                   :alias => "Other Name",
-                   :difficulty => easy
+                   name: 'Name',
+                   alias: 'Other Name',
+                   difficulty: easy
         ),
         stub_model(Mob,
-                   :name => "Name",
-                   :alias => "Other Name",
-                   :difficulty => hard
+                   name: 'Name',
+                   alias: 'Other Name',
+                   difficulty: hard
         )
     ])
   end
 
-  it "renders a list of mobs" do
+  it 'renders a list of mobs' do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Other Name".to_s, :count => 2
+
+    assert_select 'tr>td', text: 'Name'.to_s, count: 2
+    assert_select 'tr>td', text: 'Other Name'.to_s, count: 2
   end
 end
