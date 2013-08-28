be8019ac9681816f29a1a2d804e131b310422a92
Craig Read <craigabread@gmail.com>
2013-08-29 08:48:41 +1000

    Cleaning up view specs

--- spec/views/pages/show.html.erb_spec.rb
+++ spec/views/pages/show.html.erb_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-describe "pages/show.html.erb" do
+describe 'pages/show.html.erb' do
   before(:each) do
     @page = assign(:page, stub_model(Page,
-      :name => "Name",
-      :title => "Title",
-      :body => "MyText"
+      name: 'Name',
+      title: 'Title',
+      body: 'MyText'
     ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     rendered.should match(/Name/)
     rendered.should match(/Title/)
