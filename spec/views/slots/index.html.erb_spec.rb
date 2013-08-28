9dc0e296938838871def14950289517ad873a038
Craig Read <craigabread@gmail.com>
2013-08-29 08:00:13 +1000

    Cleaning up view specs

--- spec/views/slots/index.html.erb_spec.rb
+++ spec/views/slots/index.html.erb_spec.rb
@@ -1,20 +1,25 @@
 require 'spec_helper'
 
-describe "slots/index.html.erb" do
+describe 'slots/index.html.erb' do
   before(:each) do
     assign(:slots, [
       stub_model(Slot,
-        :name => "Name"
+        :name => 'Arm'
       ),
       stub_model(Slot,
-        :name => "Name"
+        :name => 'Leg'
       )
     ])
   end
 
-  it "renders a list of slots" do
+  it 'renders a list of slots' do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+
+    assert_select 'table#dataTable' do
+      assert_select 'tbody' do
+        assert_select 'tr>td', text: 'Arm', count: 1
+        assert_select 'tr>td', text: 'Leg', count: 1
+      end
+    end
   end
 end
