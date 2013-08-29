0893e6592e27fb1924d23035465a025eb20340a1
Craig Read <craigabread@gmail.com>
2013-08-29 13:21:21 +1000

    Cleaning up view specs

--- spec/views/comments/index.html.haml_spec.rb
+++ spec/views/comments/index.html.haml_spec.rb
@@ -1,25 +1,25 @@
 require 'spec_helper'
 
-describe "comments/index" do
+describe 'comments/index' do
   before(:each) do
     assign(:comments, [
       stub_model(Comment,
-        :commented_id => 1,
-        :commented_type => "Player",
-        :comment => "MyText"
+        commented_id: 1,
+        commented_type: 'Player',
+        comment: 'Comment'
       ),
       stub_model(Comment,
-        :commented_id => 1,
-        :commented_type => "Player",
-        :comment => "MyText"
+        commented_id: 1,
+        commented_type: 'Player',
+        comment: 'Comment'
       )
     ])
   end
 
-  it "renders a list of comments" do
+  it 'renders a list of comments' do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Player".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+
+    assert_select 'tr>td', text: 'Player'.to_s, count: 2
+    assert_select 'tr>td', text: 'Comment'.to_s, count: 2
   end
 end
--- spec/views/comments/show.html.haml_spec.rb
+++ spec/views/comments/show.html.haml_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
-describe "comments/show" do
+describe 'comments/show' do
   before(:each) do
     player = stub_model(Player, FactoryGirl.attributes_for(:player))
-    @comment = assign(:comment, stub_model(Comment, FactoryGirl.attributes_for(:comment, commented: player)))
+    comment_attributes = FactoryGirl.attributes_for(:comment, commented: player)
+    @comment = assign(:comment, stub_model(Comment, comment_attributes))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
 
     rendered.should match 'Player'
