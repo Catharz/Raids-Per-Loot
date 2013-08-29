9d3806417702cd14df4bd26d46109527fd9ee07b
Craig Read <craigabread@gmail.com>
2013-08-29 22:28:08 +1000

    Cleaning up observer specs

--- spec/observers/character_observer_spec.rb
+++ spec/observers/character_observer_spec.rb
@@ -5,9 +5,11 @@ describe CharacterObserver do
 
   before(:each) do
     @character = FactoryGirl.create(:character, char_type: 'm')
-    @character_type = FactoryGirl.create(:character_type, character: @character, char_type: 'm')
+    @character_type = FactoryGirl.create(:character_type,
+                                         character: @character, char_type: 'm')
     @character.stub(:last_switch).and_return(@character_type)
-    Resque.should_receive(:enqueue).at_least(1).times.with(SonyCharacterUpdater, @character.id)
+    Resque.should_receive(:enqueue).at_least(1).times.
+        with(SonyCharacterUpdater, @character.id)
   end
 
   describe 'after_create' do
@@ -26,7 +28,7 @@ describe CharacterObserver do
       subject.after_save(@character)
     end
 
-    it "doesn't store an updated character_type when the char_type doesn't change" do
+    it "doesn't try to store character_type when it hasn't changed" do
       subject.should_not_receive(:save_new_char_type)
 
       subject.after_save(@character)
