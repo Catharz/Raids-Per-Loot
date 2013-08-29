78beea56330be061bb8cb074dcf47383894f9b0c
Craig Read <craigabread@gmail.com>
2013-08-29 13:29:52 +1000

    Cleaning up view specs

--- spec/views/archetypes/edit.html.erb_spec.rb
+++ spec/views/archetypes/edit.html.erb_spec.rb
@@ -1,18 +1,16 @@
 require 'spec_helper'
 
-describe "archetypes/edit.html.erb" do
+describe 'archetypes/edit.html.erb' do
   before(:each) do
-    @archetype = assign(:archetype, stub_model(Archetype,
-      :name => "MyString"
-    ))
+    @archetype = assign(:archetype, stub_model(Archetype, name: 'Name'))
   end
 
-  it "renders the edit archetype form" do
+  it 'renders the edit archetype form' do
     render
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => archetypes_path(@archetype), :method => "post" do
-      assert_select "input#archetype_name", :name => "archetype[name]"
+
+    assert_select 'form', action: archetypes_path(@archetype), method: 'post' do
+      assert_select 'input#archetype_name', name: 'archetype[name]'
     end
   end
 end
