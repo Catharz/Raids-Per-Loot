89cf83533856c48a1fdcdf3168f1f6273c6edee5
Craig Read <craigabread@gmail.com>
2013-08-29 12:40:20 +1000

    Cleaning up view specs

--- spec/views/links/new.html.erb_spec.rb
+++ spec/views/links/new.html.erb_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
-describe "links/new.html.erb" do
+describe 'links/new.html.erb' do
   before(:each) do
     assign(:link, stub_model(Link,
-      :url => "MyString",
-      :title => "MyString",
-      :description => "MyText"
+      :url => 'MyString',
+      :title => 'MyString',
+      :description => 'MyText'
     ).as_new_record)
   end
 
-  it "renders new link form" do
+  it 'renders new link form' do
     render
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => links_path, :method => "post" do
-      assert_select "input#link_url", :name => "link[url]"
-      assert_select "input#link_title", :name => "link[title]"
-      assert_select "textarea#link_description", :name => "link[description]"
+
+    assert_select 'form', :action => links_path, :method => 'post' do
+      assert_select 'input#link_url', :name => 'link[url]'
+      assert_select 'input#link_title', :name => 'link[title]'
+      assert_select 'textarea#link_description', :name => 'link[description]'
     end
   end
 end
