4bd7399cf03278e5bfbaea3c32c8254dfa03dc25
Craig Read <craigabread@gmail.com>
2013-09-06 12:24:57 +1000

    Refactoring out complexity in the character updater

--- app/workers/sony_character_updater.rb
+++ app/workers/sony_character_updater.rb
@@ -13,22 +13,33 @@ class SonyCharacterUpdater
     raise Exception, 'Internet connection unavailable.' unless internet_connection?
 
     character_details = SonyDataService.new.character_data(character.name, 'json')
+    raise Exception, "Could not obtain character details for #{character.name}" unless valid? character_details
 
-    if character_details.nil? or character_details.empty?
-      raise Exception, "Could not obtain character details for #{character.name}"
-    else
-      archetype_name = character_details.fetch('type', {}).fetch('class')
-      unless character.archetype_name.eql? archetype_name
-        character.update_attribute(:archetype, Archetype.find_by_name(archetype_name))
-      end
-
-      if character.external_data.nil?
-        character.external_data = character.build_external_data(data: character_details)
-      else
-        character.external_data.data = character_details
-        character.external_data.save
-      end
-    end
+    update_archetype(character, character_details)
+    update_external_data(character, character_details)
+  ensure
     Character.paper_trail_on
+  end
+
+  private
+
+  def self.update_external_data(character, character_details)
+    if character.external_data.nil?
+      character.external_data = character.build_external_data(data: character_details)
+    else
+      character.external_data.data = character_details
+      character.external_data.save
+    end
+  end
+
+  def self.update_archetype(character, character_details)
+    archetype_name = character_details.fetch('type', {}).fetch('class')
+    unless character.archetype_name.eql? archetype_name
+      character.update_attribute(:archetype, Archetype.find_by_name(archetype_name))
+    end
+  end
+
+  def self.valid?(details)
+    true unless details.nil? or details.empty?
   end
 end
--- spec/workers/sony_character_updater_spec.rb
+++ spec/workers/sony_character_updater_spec.rb
@@ -10,101 +10,82 @@ describe SonyCharacterUpdater do
 
   describe '#perform' do
     it 'raises an exception if there is no internet connect' do
-      subject.should_receive(:internet_connection?).and_return(false)
+      subject.stub(:internet_connection?).and_return(false)
       expect {
         subject.perform(character.id)
       }.to raise_exception Exception, 'Internet connection unavailable.'
     end
 
     it 'raises an exception the character data is nil' do
-      subject.should_receive(:internet_connection?).and_return(true)
-      Character.should_receive(:find).and_return(character)
-      SonyDataService.any_instance.should_receive(:character_data).
-          with(character.name, 'json').and_return(nil)
+      subject.stub(:internet_connection?).and_return(true)
+      Character.stub(:find).and_return(character)
+      SonyDataService.any_instance.stub(:character_data).and_return(nil)
 
       expect {
         subject.perform(character.id)
-      }.to raise_exception Exception,
-                           'Could not obtain character details for ' +
-                               character.name
+      }.to raise_exception Exception, 'Could not obtain character details for ' + character.name
     end
 
     it 'raises an exception the character data is empty' do
-      subject.should_receive(:internet_connection?).and_return(true)
-      Character.should_receive(:find).and_return(character)
-      SonyDataService.any_instance.
-          should_receive(:character_data).with(character.name, 'json').
-          and_return({}.with_indifferent_access)
+      subject.stub(:internet_connection?).and_return(true)
+      Character.stub(:find).and_return(character)
+      SonyDataService.any_instance.stub(:character_data).and_return({}.with_indifferent_access)
 
       expect {
         subject.perform(character.id)
-      }.to raise_exception Exception,
-                           'Could not obtain character details for ' +
-                               character.name
+      }.to raise_exception Exception, 'Could not obtain character details for ' + character.name
     end
 
     context 'updating data' do
-      let(:monk_character_data) { {type: {'class' => 'Monk'}}.
-          with_indifferent_access }
+      let(:monk_character_data) { {type: {'class' => 'Monk'}}.with_indifferent_access }
       let(:monk) { Archetype.find_by_name('Monk') }
       let(:bruiser) { Archetype.find_by_name('Bruiser') }
 
       it 'does not update the character unnecessarily' do
-        subject.should_receive(:internet_connection?).and_return(true)
-        Character.should_receive(:find).and_return(character)
-        character.should_receive(:archetype_name).and_return('Monk')
-        SonyDataService.any_instance.
-            should_receive(:character_data).with(character.name, 'json').
-            and_return(monk_character_data)
+        subject.stub(:internet_connection?).and_return(true)
+        Character.stub(:find).and_return(character)
+        character.stub(:archetype_name).and_return('Monk')
+        SonyDataService.any_instance.stub(:character_data).and_return(monk_character_data)
 
         character.should_not_receive(:update_attribute)
         subject.perform(character.id)
       end
 
       it 'updates the characters archetype when necessary' do
-        subject.should_receive(:internet_connection?).and_return(true)
-        Character.should_receive(:find).and_return(character)
-        character.should_receive(:archetype_name).and_return('Bruiser')
-        SonyDataService.any_instance.
-            should_receive(:character_data).with(character.name, 'json').
-            and_return(monk_character_data)
+        subject.stub(:internet_connection?).and_return(true)
+        Character.stub(:find).and_return(character)
+        character.stub(:archetype_name).and_return('Bruiser')
+        SonyDataService.any_instance.stub(:character_data).and_return(monk_character_data)
 
         character.should_receive(:update_attribute).with(:archetype, monk)
         subject.perform(character.id)
       end
 
       it 'saves the data to external data' do
-        subject.should_receive(:internet_connection?).and_return(true)
-        Character.should_receive(:find).and_return(character)
-        character.should_receive(:archetype_name).and_return('Bruiser')
-        SonyDataService.any_instance.
-            should_receive(:character_data).with(character.name, 'json').
-            and_return(monk_character_data)
+        subject.stub(:internet_connection?).and_return(true)
+        Character.stub(:find).and_return(character)
+        character.stub(:archetype_name).and_return('Bruiser')
+        SonyDataService.any_instance.stub(:character_data).and_return(monk_character_data)
 
         character.should_receive(:update_attribute).with(:archetype, monk)
         external_data = double(ExternalData)
-        character.should_receive(:external_data).exactly(3).
-            times.and_return(external_data)
-        external_data.should_receive(:data=).
-            with({"type" => {"class" => "Monk"}})
+        character.stub(:external_data).and_return(external_data)
+        external_data.should_receive(:data=).with({"type" => {"class" => "Monk"}})
         external_data.should_receive(:save)
 
         subject.perform(character.id)
       end
 
       it 'build external data when it is nil' do
-        subject.should_receive(:internet_connection?).and_return(true)
-        Character.should_receive(:find).and_return(character)
-        character.should_receive(:archetype_name).and_return('Bruiser')
-        SonyDataService.any_instance.
-            should_receive(:character_data).with(character.name, 'json').
-            and_return(monk_character_data)
+        subject.stub(:internet_connection?).and_return(true)
+        Character.stub(:find).and_return(character)
+        character.stub(:archetype_name).and_return('Bruiser')
+        SonyDataService.any_instance.stub(:character_data).and_return(monk_character_data)
 
         character.should_receive(:update_attribute).with(:archetype, monk)
         external_data = double(ExternalData)
-        character.should_receive(:external_data).and_return(nil)
-        character.should_receive(:build_external_data).
-            with(data: {"type" => {"class" => "Monk"}}).
+        character.stub(:external_data).and_return(nil)
+        character.should_receive(:build_external_data).with(data: {"type" => {"class" => "Monk"}}).
             and_return(external_data)
         character.should_receive(:external_data=).with(external_data)
 
